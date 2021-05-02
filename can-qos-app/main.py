5ba3d771328cdee6d557f1f1ea5f90b2a9f6b789
MatJorgensen <s164151@student.dtu.dk>
2021-05-02 19:04:46 +0200

    Created function to set custom flows

--- can-qos-app/main.py
+++ can-qos-app/main.py
@@ -13,25 +13,18 @@ mininet_link_speed = 10 * 1000000
 sflow = f'http://{vm_ip_address}:{sflow_port}'
 onos = f'http://{vm_ip_address}:{onos_port}'
 
-# define custom sflow-rt flow metric
-name = 'mn_bytes'
-flow = {'value': 'bytes', 't': 2}
-r = requests.put(f'{sflow}/flow/{name}/json', data=json.dumps(flow))
-
-#name = 'mn_flow'
-#flow = {'keys': 'ipsource,ipdestination,ipprotocol,or:tcpsourceport:icmptype,or:tcpdestinationport:udpdestinationport:icmpcode', 'value': 'bytes', 't': 2}
-#r = requests.put(f'{sflow}/flow/{name}/json', data=json.dumps(flow))
-
 
 # Auxillary functions
-def flatten_list(list):
-    return [item for sublist in list for item in sublist]
+def flatten_list(nested_list):
+    return [item for sublist in nested_list for item in sublist]
+
 
 def calculate_link_utilization(ifinoctets, ifoutoctets):
     #if (ifinoctets == 0 or ifinoctets == 0):
     return max((ifinoctets * 8) / mininet_link_speed, (ifoutoctets * 8) / mininet_link_speed)
     #else:
     #    return 0
+
 
 def sort_dec_desc(dictionary):
     return {k: v for k, v in sorted(dictionary.items(), key=lambda item: item[1], reverse=True)}
@@ -45,6 +38,18 @@ def data_source_to_port(data_source):
             return metric['metricValue']
 
 
+# define custom sflow-rt flow metric
+def set_flow(name, flow):
+    r = requests.get(f'{sflow}/flow/{name}/json')
+    if r.status_code == 404:
+        requests.put(f'{sflow}/flow/{name}/json', data=json.dumps(flow))
+
+
+# define custom sflow-rt flow metric
+name = 'mn_bytes'
+flow = {'value': 'bytes', 't': 5}
+set_flow(name, flow)
+
 def calculate_metric_link_utilization():
     while True:
         r = requests.get(f'{sflow}/table/TOPOLOGY/ifname,ifinoctets,ifoutoctets/json')
@@ -53,42 +58,44 @@ def calculate_metric_link_utilization():
         for metric in metrics:
             link_utilization[f'{metric[0]["metricValue"]}'] = calculate_link_utilization(metric[1]["metricValue"], metric[2]["metricValue"])
         print(sort_dec_desc(link_utilization))
-        time.sleep(2)
+        time.sleep(1)
 
 
 def calculate_flow_link_utilization():
     # TODO: Check whether ingress and egress flows are needed...
-    r = requests.get(f'{sflow}/dump/127.0.0.1/mn_bytes/json')
-    metrics = r.json()
-    link_utilization = {}
-    for metric in metrics:
-        print(metric)
-        link_utilization[data_source_to_port(metric['dataSource'])] = metric['metricValue']
-    print(sort_dec_desc(link_utilization))
+    while True:
+        r = requests.get(f'{sflow}/dump/127.0.0.1/mn_bytes/json')
+        metrics = r.json()
+        link_utilization = {}
+        for metric in metrics:
+            if metric['dataSource'] != '0':
+                link_utilization[data_source_to_port(metric['dataSource'])] = (metric['metricValue'] * 8) / mininet_link_speed
+        print(sort_dec_desc(link_utilization))
+        time.sleep(1)
 
 
 #print(data_source_to_port('14'))
-calculate_flow_link_utilization()
-#calculate_metric_link_utilization()
+#calculate_flow_link_utilization()
+calculate_metric_link_utilization()
 
 
-
-
-
-def calculate_top_interfaces(metric):
-    r = requests.get(f'{sflow}/table/TOPOLOGY/sort:{metric}:-/json')
-
-
-    r.json()
-
-    return
-
+# Below is example of environment...
 """
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
+[
+  {
+    "link":"s1-s2",
+    "utilization":{
+      "ingress":0.463,
+      "egress":0.512
+    }
+  },
+  {
+    "link":"s1-s3",
+    "utilization":{
+      "ingress":0.327,
+      "egress":0.279
+    }
+  }
+]
 """
+
