a9daa5ad45bdf85bc75661438a8db966824916cb
MatJorgensen <s164151@student.dtu.dk>
2021-06-18 15:03:53 +0200

    Added plotting function

--- can-qos-app/main.py
+++ can-qos-app/main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
+from matplotlib import pyplot as plt
 import requests
 import json
 import time
 
 # configure connection vm, sflow and onos
-vm_ip_address = '192.168.56.103'
+vm_ip_address = '192.168.56.104'
 sflow_port = '8008'
 onos_port = '8181'
 mininet_link_speed = 10 * 1000000
@@ -49,10 +50,12 @@ class NetworkTopology:
 
     # Print JSON environment
     def print_data(self):
+        print(json.dumps(self.data, indent=2))
+
+    def print_data_larger_than_x_percent(self, x):
         for interface in self.data["interfaces"]:
-            if interface["utilization"] > 0.5:
-                print(f'{interface["interface"]}: {interface["utilization"]}, {interface["of_dpid"]}:{interface["of_port"]}')
-        #print(json.dumps(self.data, indent=2))
+            if interface["utilization"] > x:
+                print(interface)
 
     def calculate_interface_utilization(ifinoctets):
         return (ifinoctets * 8) / mininet_link_speed
@@ -60,36 +63,73 @@ class NetworkTopology:
     def sort_dictionary_descending(dictionary):
         return {k: v for k, v in sorted(dictionary.items(), key=lambda item: item[1], reverse=True)}
 
+    def generate_plot(self, data_at_time_steps):
+        plot_data = {}
+        interfaces_plotted = {}
 
+        # Initialize plot_data
+        for data_at_time_step in data_at_time_steps:
+            for interface in data_at_time_step:
+                plot_data[interface['interface']] = {}
+                interfaces_plotted[interface['interface']] = False
+        for key in plot_data:
+            for i in range(len(data_at_time_steps)):
+                plot_data[key][i] = 0.0
+
+        # Populate plot_data
+        for idx, data_at_time_step in enumerate(data_at_time_steps):
+            for interface in data_at_time_step:
+                plot_data[interface['interface']][idx] = interface['utilization']
+                if interface['utilization'] > 0.25:
+                    interfaces_plotted[interface['interface']] = True
+
+        # Generate plot
+        fig, ax = plt.subplots(figsize=(10, 4), dpi=80)
+        for interface in [*plot_data.keys()]:
+            if interfaces_plotted[interface]:
+                ax.plot([*plot_data[interface].keys()], [*plot_data[interface].values()], linewidth=2, label=interface)
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax.set_xlim(0, 50)
+        ax.set_ylim(0, 1)
+        ax.grid(axis='y', color='w')
+        ax.set_ylabel('Interface utilization')
+        ax.set_xlabel('Time steps')
+        ax.set_facecolor('#f1f1f2')
+        ax.legend()
+        plt.show()
+
+        return plot_data
+
+
+# TODO: Fix below code – currently only used for test purposes
 test = NetworkTopology()
-counter = 0
-while True:
-    print(f'------{counter}------')
-
+data_at_time_steps = []
+t_end = time.time() + 50    # 45 seconds
+while time.time() < t_end:
     test.generate_json_environment()
-    test.print_data()
-    counter += 1
+    data_at_time_steps.append(test.data["interfaces"])
     time.sleep(1)
+test.generate_plot(data_at_time_steps)
 
 
 # Below is example of environment...
-"""
-{
-  "interfaces": [
-    {
-      "interface": "s1-eth2",
-      "utilization": 6.670665866826634e-05,
-      "of_dpid": "0000000000000001",
-      "of_port": "2"
-    },
-    {
-      "interface": "s2-eth1",
-      "utilization": 0.0,
-      "of_dpid": "0000000000000002",
-      "of_port": "1"
-    }
-  ]
-}
-"""
+
+# {
+#   "interfaces": [
+#     {
+#       "interface": "s1-eth2",
+#       "utilization": 6.670665866826634e-05,
+#       "of_dpid": "0000000000000001",
+#       "of_port": "2"
+#     },
+#     {
+#       "interface": "s2-eth1",
+#       "utilization": 0.0,
+#       "of_dpid": "0000000000000002",
+#       "of_port": "1"
+#     }
+#   ]
+# }
 
 
